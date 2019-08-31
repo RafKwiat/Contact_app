94e9936f6d5bae9f3d42c18bfab11d49bc3b40a6
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-08-31 22:50:32 +0200

    Final

--- components/Contact.js
+++ components/Contact.js
@@ -12,14 +12,14 @@ var Contact = React.createClass({
             React.createElement('div', {className: 'contactItem'},
                 React.createElement('img', {
                     className: 'contactImage', 
-                    src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
+                    src: 'icon.png'
                 }),
-                React.createElement('p', {className: 'contactLabel'}, 'Imię' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactEmail', href: 'mailto' + this.props.item.email},
+                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {className: 'contactEmail', href: 'mailto: ' + this.props.item.email},
                     this.props.item.email),
             )
-        )
-    },
+        );
+    }
 });
 
